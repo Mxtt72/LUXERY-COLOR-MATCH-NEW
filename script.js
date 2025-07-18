34179e9f74ed78fea81d992f0d162fbbf755e490
Mxtt72 <matthew@gillart.com>
2025-07-18 20:55:59 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const imageUpload = document.getElementById('imageUpload');
+
+const canvas = document.getElementById('imageCanvas');
+
+const ctx = canvas.getContext('2d');
+
+
+const scanBtn = document.getElementById('scanBtn');
+
+
+
